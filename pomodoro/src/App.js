adc7609fab35b80fceb6932f37b4da7cea5d04a5
Carolien <heymanscarolien@gmail.com>
2019-04-02 15:19:34 +0200

    countdown +-

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line
 import React, { Component } from 'react';
 import './App.css';
 
@@ -7,30 +8,53 @@ class App extends React.Component {
         this.state = {
             timerStarted: false,
             timerStopped: true,
-            minutes: 0,
+            minutes: 25,
             seconds: 0,
             
         }}
+
     handleTimerStart(e) {
-            e.preventDefault();
+                 e.preventDefault();
             if(this.state.timerStopped) {
               this.timer = setInterval(() => {
                   this.setState({timerStarted: true, timerStopped: false});
                   if(this.state.timerStarted) {
-                    if(this.state.seconds >= 60) {
-                      this.setState((prevState) => ({ minutes: prevState.minutes + 1, seconds: 0}));
+                    if(this.state.seconds === 0) {
+                      this.setState((prevState) => ({ minutes: prevState.minutes - 1, seconds: 60}));
                     }
-                    this.setState((prevState) => ({ seconds: prevState.seconds + 1 }));
+                    this.setState((prevState) => ({ seconds: prevState.seconds - 1 }));
                   }
           
               }, 1000);
             }
+      
           }
-          handleTimerStop() {
+    handleTimerStop() {
             this.setState({timerStarted: false, timerStopped: true});
             clearInterval(this.timer);
           }
-          
+
+    handlePlusTime(){
+      if(this.state.timerStopped) {
+      this.setState({ minutes:this.state.minutes + 1});
+          }
+        }
+    handleMinTime(){
+      if(this.state.timerStopped) {
+      this.setState({ minutes:this.state.minutes - 1});
+            }
+          }
+
+        handleReset(){
+          this.setState({
+          timerStarted: false,
+          timerStopped: true,
+          seconds: 0,
+          minutes: 25,
+               });
+        clearInterval(this.timer);
+      }
+    
     render() {
         return (
          <div className="container">
@@ -46,12 +70,14 @@ class App extends React.Component {
                 Timer</button>
                <button onClick={this.handleTimerStop.bind(this)}>Stop 
                 Timer</button>
-                <button >Reset</button>
+                <button onClick={this.handlePlusTime.bind(this)} >+</button>
+                <button onClick={this.handleMinTime.bind(this)} >-</button>
+                <button onClick={this.handleReset.bind(this)} >Reset</button>
              </div>
+      
            </div>
          </div>
      );
      }
     }
-
 export default App;
