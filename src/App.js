b7b47ff6a34912eb3e094b2b9b0155f38fe72baa
Carolien <heymanscarolien@gmail.com>
2019-04-03 11:22:40 +0200

    layout

--- src/App.js
+++ src/App.js
@@ -1,108 +1,104 @@
 // eslint-disable-next-line
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            timerStarted: false,
-            timerStopped: true,
-            minutes: 25,
-            seconds: 0
-        }
-    }
-    handleReset() {
-        this.setState({timerStarted: false, timerStopped: true, seconds: 0, minutes: 25});
-        clearInterval(this.timer);
-    }
-    handleTimerStop() {
-        this.setState({timerStarted: false, timerStopped: true});
-        clearInterval(this.timer);
-    }
-    handleTimerStart(e) {
-        e.preventDefault();
-        if (this.state.timerStopped) {
-            this.timer = setInterval(() => {
-                this.setState({timerStarted: true, timerStopped: false});
-                if (this.state.timerStarted) {
-                    if (this.state.seconds === 0) {
-                        this.setState((prevState) => ({
-                            minutes: prevState.minutes - 1,
-                            seconds: 60
-                        }));
-                    }
-                    this.setState((prevState) => ({
-                        seconds: prevState.seconds - 1
-                    }));
-                    if (this.state.minutes === 0 && this.state.seconds === 0) {
-                      alert("take a break!");
-                      this.handleReset();
-                  }
-                }
+	constructor(props) {
+		super(props);
+		this.state = {
+			timerStarted: false,
+			timerStopped: true,
+			minutes: 25,
+			seconds: 0
+		};
+	}
+	handleReset() {
+		this.setState({ timerStarted: false, timerStopped: true, seconds: 0, minutes: 25 });
+		clearInterval(this.timer);
+	}
+	handleTimerStop() {
+		this.setState({ timerStarted: false, timerStopped: true });
+		clearInterval(this.timer);
+	}
+	handleTimerStart(e) {
+		e.preventDefault();
+		if (this.state.timerStopped) {
+			this.timer = setInterval(() => {
+				this.setState({ timerStarted: true, timerStopped: false });
+				if (this.state.timerStarted) {
+					if (this.state.seconds === 0) {
+						this.setState((prevState) => ({
+							minutes: prevState.minutes - 1,
+							seconds: 60
+						}));
+					}
+					this.setState((prevState) => ({
+						seconds: prevState.seconds - 1
+					}));
+					if (this.state.minutes === 0 && this.state.seconds === 0) {
+						alert('take a break!');
+						this.handleReset();
+					}
+				}
+			}, 1000);
+		}
+	}
 
-            }, 1000);
-        }
+	handlePlusTime() {
+		if (this.state.timerStopped) {
+			this.setState({
+				minutes: this.state.minutes + 1
+			});
+		}
+	}
+	handleMinTime() {
+		if (this.state.timerStopped) {
+			this.setState({
+				minutes: this.state.minutes - 1
+			});
+		}
+	}
 
-    }
+	render() {
+		let button;
+		if (this.state.timerStopped) {
+			button = (
+				<button className="btn btn-primary" onClick={this.handleTimerStart.bind(this)}>
+					Start Timer
+				</button>
+			);
+		} else {
+			button = (
+				<button className="btn btn-primary" onClick={this.handleReset.bind(this)}>
+					Reset
+				</button>
+			);
+		}
+		return (
+			<div className="container">
+				<h2 className="text-center">Pomodoro Timer</h2>
+				<div className="timer-container">
+					<div className="timer-controls">
+						<button className="btn btn-secondary" onClick={this.handlePlusTime.bind(this)}>
+							+
+						</button>
+					</div>
+					<div className="current-timer">{this.state.minutes + ':' + this.state.seconds}</div>
 
-    handlePlusTime() {
-        if (this.state.timerStopped) {
-            this.setState({
-                minutes: this.state.minutes + 1
-            });
-        }
-    }
-    handleMinTime() {
-        if (this.state.timerStopped) {
-            this.setState({
-                minutes: this.state.minutes - 1
-            });
-        }
-    }
-
-    render() {
-
-let button;
-if(this.state.timerStopped){
-button = <button
-onClick={this
-.handleTimerStart
-.bind(this)}>Start Timer</button>;
-}else{
-button = <button onClick={this.handleReset.bind(this)}>Reset</button>;
-} 
-        return (
-            <div className="container">
-                <h2 className="text-center">Tomato Timer</h2>
-                <div className="timer-container">
-                    <div className="current-timer">
-                        <div id="minutes">
-                            {"Minutes: " + this.state.minutes}</div>
-
-                        <div id="seconds">
-                            {"Seconds: " + this.state.seconds}</div>
-                    </div>
-                    <div className="timer-controls">
-                        {button}
-                        <button
-                            onClick={this
-                            .handleTimerStop
-                            .bind(this)}>Stop Timer</button>
-                        <button
-                            onClick={this
-                            .handlePlusTime
-                            .bind(this)}>+</button>
-                        <button
-                            onClick={this
-                            .handleMinTime
-                            .bind(this)}>-</button>
-                       
-                    </div>
-
-                </div>
-            </div>
-        );
-    }
+					<div className="timer-controls">
+						<button className="btn btn-secondary" onClick={this.handleMinTime.bind(this)}>
+							-
+						</button>
+					</div>
+				</div>
+				<div className="controls">
+					{button}
+					<button className="btn btn-primary" onClick={this.handleTimerStop.bind(this)}>
+						Stop Timer
+					</button>
+				</div>
+			</div>
+		);
+	}
 }
 export default App;
