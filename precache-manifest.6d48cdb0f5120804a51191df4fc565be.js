48c0bbaeddb53fb52f252b8d31ff8a7dd3a6ea80
Carolien <heymanscarolien@gmail.com>
2019-04-05 11:14:33 +0200

    Updates

---
+++ precache-manifest.6d48cdb0f5120804a51191df4fc565be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7f6af380a09cd767ec6ee32c19ff439",
+    "url": "/challenge-react-pomodoro/static/media/churchchimedanielsimon.f7f6af38.mp3"
+  },
+  {
+    "revision": "f971cbf4af2cd6b7a26d",
+    "url": "/challenge-react-pomodoro/static/js/runtime~main.0c2dd5de.js"
+  },
+  {
+    "revision": "f570711397ecbc570bf4",
+    "url": "/challenge-react-pomodoro/static/js/main.f9b373f8.chunk.js"
+  },
+  {
+    "revision": "83a690cbecc05f026a3a",
+    "url": "/challenge-react-pomodoro/static/js/2.9969aa60.chunk.js"
+  },
+  {
+    "revision": "f570711397ecbc570bf4",
+    "url": "/challenge-react-pomodoro/static/css/main.8908f4ba.chunk.css"
+  },
+  {
+    "revision": "83a690cbecc05f026a3a",
+    "url": "/challenge-react-pomodoro/static/css/2.43c9d499.chunk.css"
+  },
+  {
+    "revision": "383ca9beb658dec7a5a5a0655ceb1015",
+    "url": "/challenge-react-pomodoro/index.html"
+  }
+];
